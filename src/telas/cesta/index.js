42a7601f79b422e20c60a62864427b59279dd5f9
tontitor <ailtonarrudatec@gmailcom>
2022-08-27 16:48:04 -0300

    refactoring

--- src/telas/cesta/index.js
+++ src/telas/cesta/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 
-
-import Texto from '../../componentes/Texto';
 import Topo from './componentes/Topo'
 import Detalhes from './componentes/Detalhes';
-
 
 
 export default function Cesta() {
     return <>
         <Topo />
         <View style={estilos.cesta}>
-        <Detalhes />
+            <Detalhes />
         </View>
     </>    
 }
